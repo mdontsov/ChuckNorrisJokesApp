133608e1aa128424a9befe4b536bf5de9c3f0afd
maksim.dontsov <maksim.dontsov@elisa.ee>
2018-06-11 08:17:41 +0300

    Initial commit

---
+++ src/main/java/com/example/ChuckNorrisJokesApp/ChuckNorrisJokesAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.ChuckNorrisJokesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokesAppApplication.class, args);
+	}
+}
